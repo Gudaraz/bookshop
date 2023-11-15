154ed4043712c80c2fd2837bc93c58cec19b8e24
Rafael Montaner <rafa@MacBook-Pro-de-Rafael.local>
2023-11-15 00:32:08 -0300

    second commit

---
+++ bookshop/src/Components/Banner/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Banner.css"
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="banner-container">
+        <h1>RAFA Books Shop</h1>
+        <p><i>The best place to find the book you are looking for</i></p>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
